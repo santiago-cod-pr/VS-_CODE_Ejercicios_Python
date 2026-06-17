ef99d50934400a71e825d2afdc9bf6bbc946bea2
santiago-cod-pr <santiagomartinezc2007gmail.com>
2026-06-17 08:25:58 -0600

    Ejercicios_con_Ciclo_While

--- PRACTICAS PYTHON 2/Ejercicio_4.py
+++ PRACTICAS PYTHON 2/Ejercicio_4.py
@@ -17,3 +17,28 @@ formula =  N * (N + 1)//2
 print(f"Verificacion con formula: {formula}")
 
 #Tu turno: Reescribe el ejercicio usando for en lugar de while. ¿Cuál versión es más natural para este problema? Explica por qué con tus palabras.
+
+
+
+# Ejercicio - While para validar entrada: el patron de reinicio
+
+nota = float(input("Calificacion (0 - 10): "))
+             
+while nota <0 or nota > 10:
+    print("Calificacion invalida. Debe ser entre 0 y 10.")
+    nota = float(input("Calificacion (0-10)"))
+
+print(f"Calificacion Registrada: {nota:.1f}")
+print("")
+
+
+# While True + break
+while True:
+    edad = int(input("Tu edad(1-80)"))
+    if 1 <= edad <= 80:
+        break
+    print("Edad invalida, Intenta de nuevo.")
+
+print(f"Edad Registrada: {edad}")
+
+#Tu turno: Crea un programa que pida al usuario adivinar un número secreto (define tú el número con una constante). Con while, sigue pidiendo hasta que lo adivine e imprime cuántos intentos necesitó.
