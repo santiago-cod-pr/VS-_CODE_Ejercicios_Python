32aa39da4682096e2e528a18d16caafc9a83ea28
santiago-cod-pr <santiagomartinezc2007gmail.com>
2026-06-21 14:19:20 -0600

    Ejercicio_2 - sistema de acceso

--- PRACTICAS PYTHON 2/Ejercicio_2_Sistema_de_Acceso.py
+++ PRACTICAS PYTHON 2/Ejercicio_2_Sistema_de_Acceso.py
@@ -1,9 +1,33 @@
 #SANTIAGO MARTINEZ CAAMAL | TI21
+
+
+#Ejercicio
 #Condicional anidada: Sistema de acceso
-#Usuario y contrasena correcta. Si el Usuario falla, ni siquiera revisa la contrasena
+#Usuario correcto Y contraseña correcta. Si el usuario falla, ni siquiera revisa #la contraseña.
 
+# ── Ejercicio 5: if anidado — sistema de acceso ───────────────
+USUARIO_CORRECTO    = "admin"
+CONTRASENA_CORRECTA = "1234"
 
-#Tu turno: Reescribe el Ejercicio  usando una sola condición con and en lugar del if anidado. Luego compara las dos versiones: ¿cuál da mensajes de error más específicos y por qué?
+usuario    = input("Usuario: ")
+contrasena = input("Contraseña: ")
+
+if usuario == USUARIO_CORRECTO:
+    # Solo llegamos aquí si el usuario es correcto
+    if contrasena == CONTRASENA_CORRECTA:
+        print("Acceso concedido. Bienvenido.")
+    else:
+        print("Usuario correcto pero contraseña incorrecta.")
+else:
+    print("Usuario no reconocido.")
+
+#_______________________________________________________________________________________________________________________________________________
+
+#Tu turno: Reescribe el Ejercicio  usando una sola condición con and en lugar del if anidado.
+# Luego compara las dos versiones: ¿cuál da mensajes de error más específicos y por qué?
+
+#RESPUESTA: El primer ejercicio de if anidado da el mensaje mas especifico, ya que da a conocer al usuario donde tuvo el error,
+#indica si fue el usuario o la contrasena donde esta el detalle, en cambio la condicion "and", no nos da donde se podria encontar el error.
 
 USUARIO_CORRECTO = "santiago"
 CONTRASEÑA_CORRECTA = "2007"
