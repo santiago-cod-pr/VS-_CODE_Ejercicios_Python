ac9d270fd1d245be13c87c3fc64e7ad2f0fbb560
santiago-cod-pr <santiagomartinezc2007gmail.com>
2026-06-21 10:03:04 -0600

    Ejercicio_2 - sistema de acceso

--- PRACTICAS PYTHON 2/Ejercicio_2_Sistema_de_Acceso.py
+++ PRACTICAS PYTHON 2/Ejercicio_2_Sistema_de_Acceso.py
@@ -6,17 +6,17 @@
 #Tu turno: Reescribe el Ejercicio  usando una sola condición con and en lugar del if anidado. Luego compara las dos versiones: ¿cuál da mensajes de error más específicos y por qué?
 
 USUARIO_CORRECTO = "santiago"
-CONTRASENA_CORRECTA = "2007"
+CONTRASEÑA_CORRECTA = "2007"
 
 print("=========      LOGIN     ===========")
 print("====================================")
-print("=== INGRESE USURIO Y CONTRASENA ===")
+print("=== INGRESE USUARIO Y CONTRASEÑA ===")
 print("====================================")
 print("")
 usuario = input( "INGRESE SU USUARIO: ")
-contrasena = input("INGRESE SU CONTRASENA: ")
+contrasena = input("INGRESE SU CONTRASEÑA: ")
 
-if usuario == USUARIO_CORRECTO and contrasena == CONTRASENA_CORRECTA:
+if usuario == USUARIO_CORRECTO and contrasena == CONTRASEÑA_CORRECTA:
     print("ACCESO CONCEDIDO. BIENVENIDO")
 else:
-    print("USUARIO O CONTRASENA INCORRECTOS.")
+    print("USUARIO O CONTRASEÑA INCORRECTOS.")
